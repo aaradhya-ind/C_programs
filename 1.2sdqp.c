3693976fd552f697a80671467eabac6da1906b9a
aaradhya-ind <aaradhyadz01@gmail.com>
2025-10-05 15:50:41 +0530

    1.2sdqp.c

--- 1.2sdqp.c
+++ 1.2sdqp.c
@@ -2,21 +2,22 @@
  int main() {
      int a, b, sum, product, difference, quotient;
 
-     printf(Enter first number:);
-     scanf(%d, &a);
+     printf("Enter first number:");
+     scanf("%d", &a);
 
-     printf(Enter second number:);
-     scanf(%d, &b);
+     printf("Enter second number:");
+     scanf("%d", &b);
 
     sum = a + b;
       product = a * b;
         difference = a - b;
-         quotient = a / b;
+          quotient = a / b;
         
-        printf(The sum is: %d\n, sum);
-        printf(The product is: %d\n, product);
-        printf(The difference is: %d\n, difference);
-        printf(The quotient is: %d\n, quotient);
+        printf("The sum is: %d\n", sum);
+        printf("The product is: %d\n", product);
+        printf("The difference is: %d\n", difference);
+        printf("The quotient is: %d\n", quotient);
 
         return 0;
     }
+  
